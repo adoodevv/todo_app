361cc3fc426e00f17525c4ea58f2b7be77f2eaae
[Jonathan Darko Adoo] <[adoojonathan412@gmail.com]>
2023-10-12 15:15:54 +0000

    added done checklist functionality and starte4d styling

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const taskInput = document.getElementById("task-input");
 const addBtn = document.getElementById("add-button");
 const taskList = document.getElementById("task-list");
+const doneList = document.getElementById("done-list");
 
 addBtn.addEventListener("click", addTask);
 
@@ -9,7 +10,25 @@ function addTask() {
     if (taskText.trim() !== "") {
         const taskItem = document.createElement("li");
         taskItem.innerText = taskText;
+
+        const checkBox = document.createElement("input");
+        checkBox.type = "checkbox";
+        checkBox.addEventListener("change", moveTask);
+        taskItem.appendChild(checkBox)
+
         taskList.appendChild(taskItem);
         taskInput.value = "";
+    }
+}
+
+function moveTask(event) {
+    const taskItem = event.target.parentElement;
+    const isChecked = event.target.checked;
+
+    if (isChecked) {
+        doneList.appendChild(taskItem);
+    }
+    else {
+        taskList.appendChild(taskItem);
     }
 }
